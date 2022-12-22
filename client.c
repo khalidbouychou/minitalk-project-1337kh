ba872d0c9c8354446f2bff005e52e38e8bdbe875
khalid bouychou <khalidbouychou22.com>
2022-12-22 20:21:27 +0100

    done

--- client.c
+++ client.c
@@ -6,13 +6,13 @@
 /*   By: khbouych <khbouych@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 15:02:33 by khbouych          #+#    #+#             */
-/*   Updated: 2022/12/18 15:12:47 by khbouych         ###   ########.fr       */
+/*   Updated: 2022/12/22 19:57:01 by khbouych         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	len_data;
+int		len_data;
 
 void	ft_char_to_binary(char c, int pid)
 {
@@ -35,23 +35,23 @@ void	ft_respond(int user_signal)
 {
 	static int	count;
 
-	if (++count == (len_data) * 8 && user_signal == SIGUSR1)
+	if (++count == (len_data)*8 && user_signal == SIGUSR1)
 	{
-		ft_printf("%d  { Signals sent successfully }      \n", count);
-		ft_printf("%d  { Characters sent successfully }\n", len_data);
+		ft_printf("%d  Signals sent successfully \n", count);
+		ft_printf("%d  Characters sent successfully \n", len_data);
 	}
 }
 
 int	main(int ac, char **av)
 {
-	int	index;
-	int	server_pid;
+	int index;
+	int server_pid;
 
 	if (ac != 3)
-		return (1);
+		return (-1);
 	index = 0;
-	len_data = strlen(av[2]);
-	server_pid = atoi(av[1]);
+	len_data = ft_strlen(av[2]);
+	server_pid = ft_atoi(av[1]);
 	signal(SIGUSR1, ft_respond);
 	while (av[2][index])
 		ft_char_to_binary(av[2][index++], server_pid);
--- server.c
+++ server.c
@@ -6,40 +6,35 @@
 /*   By: khbouych <khbouych@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 15:02:43 by khbouych          #+#    #+#             */
-/*   Updated: 2022/12/18 18:04:59 by khbouych         ###   ########.fr       */
+/*   Updated: 2022/12/22 20:19:08 by khbouych         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	ft_reset(char *a, int *b)
-{
-	*a = 0;
-	*b = 0;
-}
-
-void	ft_signal_handler(int signal_received, siginfo_t *info_signal ,void *context)
+void	ft_signal_handler(int signal_received, siginfo_t *info_signal )
 {
 	static char	c;
 	static int	bits;
 	static int	current_pid;
 	int			new_pid;
 
-	(void)context;
 	if (!current_pid)
 		current_pid = info_signal->si_pid;
 	new_pid = info_signal->si_pid;
 	if (new_pid != current_pid)
 	{
 		current_pid = new_pid;
-		ft_reset(&c, &bits);
+		c = 0;
+		bits = 0;
 	}
 	c = c | (signal_received == SIGUSR1);
 	bits++;
 	if (bits == 8)
 	{
-		ft_printf("%c", c);
-		ft_reset(&c, &bits);
+		ft_printf("%c", c); 
+		c = 0;
+		bits = 0;
 	}
 	c <<= 1;
 	usleep(100);
@@ -49,10 +44,11 @@ void	ft_signal_handler(int signal_received, siginfo_t *info_signal ,void *contex
 int	main(void)
 {
 	struct sigaction action;
- 
-	action.sa_sigaction = ft_signal_handler;
-	action.sa_flags = SA_SIGINFO;
-	sigemptyset(&action.sa_mask);
+	
+	action.sa_sigaction =(void *)ft_signal_handler;
+	//action.sa_flags = SA_SIGINFO;
+
+	// signal(SIGTSTP,&ft_signal_handler);
 	ft_printf(" *** This My PID  ==>[ %d ] *** \n", getpid());
 	while (1)
 	{
